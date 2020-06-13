9c5961904bbc0622dba424f007cb99c2b0195f8f
Milan Andreew <milanand@linux.fritz.box>
2020-06-13 21:09:01 +0200

    Lesson 6

---
+++ lesson06/age_checker.py
@@ -0,0 +1,7 @@
+age = int(input())
+if age < 0:
+    print("You are a time traveller")
+elif age <= 17:
+    print("Too young to vote")
+else:
+    print("You can vote")
---
+++ lesson06/nested_ifs.py
@@ -0,0 +1,7 @@
+# Variables password, hour are predefined
+if password=='openSesame':
+    print('User logged on.')
+    if hour>17:
+        print('Good evening!')
+    print('Enter a command:')
+# Output 'User logged on' is not possible
