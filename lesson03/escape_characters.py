78b2a8e2f97a493aa0860c139de6deb3eedda305
Milan Andreew <milanand@linux.fritz.box>
2020-06-13 20:05:08 +0200

    Lesson 3

---
+++ lesson03/escape_characters.py
@@ -0,0 +1,4 @@
+print("Backslashes \\ and single quotes \' and double quotes \" and pound \
+signs # are awesome!")
+# Output: Backslashes \ and single quotes ' and double quotes " and pound
+# signs # are awesome!" (No line break)
