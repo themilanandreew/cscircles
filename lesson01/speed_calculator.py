ac8960ebdf0551081446cd6d9436e93fc89794ff
Milan Andreew <milanand@linux.fritz.box>
2020-06-13 19:02:23 +0200

    add speed_calculator.py

---
+++ lesson01/speed_calculator.py
@@ -0,0 +1,5 @@
+# uphillDistance, downhillDistance, uphillTime, downhillTime are predefined
+totalDistance = uphillDistance + downhillDistance
+totalTime = uphillTime + downhillTime
+averageSpeed = totalDistance / totalTime
+print(averageSpeed)
