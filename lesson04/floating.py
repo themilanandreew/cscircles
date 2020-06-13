afa1601ca8bdd6afddf8976d9d62f0af94b241ba
Milan Andreew <milanand@linux.fritz.box>
2020-06-13 20:16:08 +0200

    Lesson 4

---
+++ lesson04/floating.py
@@ -0,0 +1,6 @@
+x = 1.5
+y = 2
+z = x * y
+print(x, y, z)
+print(type(x), type(y), type(z))
+# Output: 1.5 2 3 \n <class 'float'> <class 'int'> <class 'float'>
---
+++ lesson04/tasty_typecasting.py
@@ -0,0 +1,4 @@
+# inputStr is predefined
+inputStr = float(inputStr)
+a = inputStr ** 2
+print(int(a/100))
